7c3e917268ef0e5dfc6ca134d74de0348797ed43
paulk <paulkotsis@hotmail.com>
2022-04-14 01:07:15 +0200

    Constuctors

--- src/main/java/MultimediaFile.java
+++ src/main/java/MultimediaFile.java
@@ -6,7 +6,7 @@ import java.util.Hashtable;
 public class MultimediaFile {
 
     String AbsolutePath;
-    String VideoName;
+    String FileName;
     String ChannelName;
     String DateCreated;
     String Length;
@@ -14,32 +14,51 @@ public class MultimediaFile {
     String FrameWidth;
     String FrameHeight;
     ArrayList<String> Hashtags;
-    byte[] VideoFileChunk;
+    byte[] FileChunk;
     boolean IsLast = false;
 
 
-    //Constructor
-    public MultimediaFile(String videoName, String channelName, String dateCreated, String length, String framerate,
-                          String frameWidth, String frameHeight , ArrayList<String> hashtags, byte[] videoFileChunk , String absolutePath) {
-        this.VideoName = videoName;
+    //Video Constructor
+    public MultimediaFile(String videoName, String channelName, String dateCreated, String length, String Framerate,
+                          String frameWidth, String frameHeight , ArrayList<String> hashtags, byte[] FileChunk , String absolutePath) {
+        this.FileName = videoName;
         this.ChannelName = channelName;
         this.DateCreated = dateCreated;
         this.Length = length;
-        this.Framerate = framerate;
+        this.Framerate = Framerate;
         this.FrameWidth = frameWidth;
         this.FrameHeight = frameHeight;
         this.Hashtags = hashtags;
-        this.VideoFileChunk = videoFileChunk;
+        this.FileChunk = FileChunk;
         this.AbsolutePath = absolutePath;
     }
 
-    public MultimediaFile(byte[] videoFileChunk , String fileName , String dateCreated){
-        this.VideoFileChunk = videoFileChunk;
-        this.VideoName =fileName;
+    public MultimediaFile(byte[] FileChunk , String fileName , String dateCreated){
+        this.FileChunk = FileChunk;
+        this.FileName =fileName;
         this.DateCreated = dateCreated;
     }
+
+    //Photo constructor
+    public MultimediaFile(byte[] FileChunk, String fileName, String channelName,String profileName, String dateCreated, String frameWidth, String frameHeight ){
+        this.FileChunk = FileChunk;
+        this.FileName =fileName;
+        this.ChannelName = channelName;
+        this.DateCreated = dateCreated;
+        this.FrameWidth = frameWidth;
+        this.FrameHeight = frameHeight;
+    }
+
+    //Text constructor
+    public MultimediaFile(byte[] FileChunk, String fileName, String channelName,String profileName, String dateCreated){
+        this.FileChunk = FileChunk;
+        this.FileName =fileName;
+        this.ChannelName = channelName;
+        this.DateCreated = dateCreated;
+    }
+
     public String getVideoName() {
-        return VideoName;
+        return FileName;
     }
     public void setHashtags(ArrayList<String> hashtags) {
         this.Hashtags= hashtags;
@@ -58,6 +77,6 @@ public class MultimediaFile {
     }
     public boolean IsLast(){return this.IsLast;}
     public byte[] getVideoFileChunk() {
-        return VideoFileChunk;
+        return FileChunk;
     }
 }
