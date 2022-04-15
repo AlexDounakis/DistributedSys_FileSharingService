5e517dfc140daa8e887e9266b3870e02cb768487
paulk <paulkotsis@hotmail.com>
2022-04-15 17:00:34 +0200

    Value (not finished yet)

---
+++ src/main/java/Value.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Value implements Serializable {
+
+    private MultimediaFile multimediaFile;
+    private Address address;
+
+    public Value (MultimediaFile m) {
+        this.multimediaFile = m;
+    }
+
+    public Value (Address address) {
+        this.address = address;
+    }
+
+    public MultimediaFile getValue() {return multimediaFile;}
+
+    public Address getAddress() {return address;}
+
+    //@Override
+    //public String toString() {return this.multimediaFile +" , "+this.address;}
+}
