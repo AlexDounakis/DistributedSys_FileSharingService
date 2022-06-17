e7266dd3425a6259a91bb6186dd5e5dde30574cb
Michael Papapetros <michalispap99@github.com>
2022-06-17 21:35:01 +0300

    Change files

--- src/main/java/IConsumer.java
+++
@@ -1,7 +0,0 @@
-public interface IConsumer extends INode {
-
-    void disconnect(String s);
-    void register(String s);
-    void showConversationData(String s, Value v);
-
-}
--- src/main/java/SenderType.java
+++
@@ -1,5 +0,0 @@
-public enum SenderType {
-    BROKER,
-    PUBLISHER,
-    CONSUMER
-}
